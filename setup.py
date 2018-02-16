ed83e6c217d18b52ef4a466d9f82a5d96b8b4356
jerakin <the_rensvind@hotmail.com>
2018-02-16 16:15:55 +0100

    removed unsupported python versions from the pypi setup file

--- setup.py
+++ setup.py
@@ -40,9 +40,6 @@ setup(
 
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
